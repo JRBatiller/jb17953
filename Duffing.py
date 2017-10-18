f5617c346e430187b03f3cd9e57063f6b00a7a43
Jose Ramon Batiller <jb17953@my.bristol.ac.uk>
2017-10-18 11:08:50 +0100

    I made a new file

---
+++ Duffing.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct 18 11:03:47 2017
+
+@author: Joby
+"""
+"""
+Simulate the nonlinear Duffing equation u¨+2ξ u˙+u+u
+3 = Γsin(ωt) for ξ = 0.05, Γ = 0.2 and 0.5 ≤
+ω ≤ 1.5.
+• What behaviour do you see in the long-time limit?
+• Isolate a periodic orbit. What are its starting conditions? What is its period?
+1
+• This will provide testing data for your numerical methods.
+– Always test your code against known results or you will come unstuck!
+"""
