03b5a739a750bab53dec647db6e5e1072daf5b15
Jose Ramon Batiller <jb17953@my.bristol.ac.uk>
2017-10-18 12:05:24 +0100

    corrected du_dt. Plot is a wave in a wave

--- Duffing.py
+++ Duffing.py
@@ -29,32 +29,47 @@ from scipy.integrate import odeint
 from scipy import stats
 
 
-#def du1dt(u,t): this is a bad name
+#def dudt(u,t): this is a bad name
 
-def du1_dt(u1,u2,t):
-    du1=u2
-    return du1
 
-def du2_dt(u1,u2,t):
-    du2=gamma*np.sin(omega*t)-2*epsilon*u2-u1**3    
-    return du2
 
 def du_dt(u,t):
-    du=[u[1], gamma*np.sin(omega*t)-2*epsilon*u[1]-u[0]**3]
+    #takes a vector,returns a vector
+    du=[u[1], gamma*np.sin(omega*t)-2*epsilon*u[1]-u[0]-u[0]**3]
     return du    
     
+if __name__ == "__main__":
+    epsilon=0.05
+    gamma=0.2
+    #Do i just set any value of omega
+    omega=1.0
+    #Do I just set initial conditions
+    U0=np.array([0,0])
+    ts=np.linspace(0,1000*np.pi,200)
+    Us=odeint(du_dt,U0,ts)
+    us=Us[:,0]    
+    dus=Us[:,1]
 
-epsilon=0.05
-gamma=0.2
-#Do i just set any value of omega
-omega=1.0
-#Do I just set initial conditions
-U0=np.array([0,0])
-ts=np.linspace(0,50*np.pi,200)
-Us=odeint(du_dt,U0,ts)
-ys=Us[:,0]    
 
-plt.xlabel("t")
-plt.ylabel("u")
-plt.title("Duffing")
-plt.plot(ts,ys);
+    fig1=plt.figure()
+    ax1=fig1.add_subplot(1,1,1)
+    ax1.set_xlabel("t")
+    ax1.set_ylabel("u")
+    #plt.title("Duffing")
+    ax1.set_xticks([np.pi*100*t for t in range(20)])
+    ax1.set_xticklabels([
+        r'$0$',
+        r'$100\pi$',
+        r'$200\pi$',
+        r'$300\pi$',
+        r'$400\pi$',
+        r'$500\pi$',
+        r'$600\pi$',
+        r'$700\pi$',
+        r'$800\pi$',
+        r'$900\pi$',
+        r'$1000\pi$',
+    ], fontsize='medium')
+    ax1.plot(ts,us)
+    ax1.plot(ts,dus)
+    fig1.savefig('Duffing Plot.svg')
